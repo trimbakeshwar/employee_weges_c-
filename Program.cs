f23aad97cf71b155ddb21e526f887ea10098f1e1
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-31 12:32:01 +0530

    Added:-welcome to employee weges

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employeeWeges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wegs");   
+        }
+    }
+}
