3faaa710f204b7e5994b729613ca5d8bd2f0d240
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-31 18:13:03 +0530

    Added:-employee is present or absent

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ namespace employeeWeges
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to employee wegs");   
+            Random rand = new Random();
+            int num = rand.Next(2);
+            if (num == 0)
+            {
+                Console.WriteLine("present");
+            }
+            else
+            {
+                Console.WriteLine("absent");
+            }
         }
     }
 }
