95b18d5e1eea0b5d9941c07d871cf8efcdce314a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-01 13:45:57 +0530

    Added:-to calculate monthly weges

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
     public class employeeWeges
-    {   
-        
-        public int checkAttendance()
+    {
+    int EMPLOYEE_WEGES;
+    int employee_Monthly_weges;
+    int countDays;
+    int WORKING_DAYS;
+    public employeeWeges()
+    {
+        EMPLOYEE_WEGES=0;
+        employee_Monthly_weges=0;
+        countDays = 0;
+        WORKING_DAYS = 20;
+    }
+    public int checkAttendance()
         {
             Random rand = new Random();
             int num = rand.Next(2);
@@ -20,54 +30,58 @@
         {
             return EMPLOYEE_WEGES_PER_HOUR * FULL_DAY_HOUR; 
         }
-        
-    }
-
-    class MainD
-    {
-    //private static int EMPLOYEE_PRESENT_OR_ABSENT;
-        public static void Main(string[] args)
+      
+        public int monthlyWeges(int EMPLOYEE_WEGES_PER_HOUR, int FULL_DAY_HOUR, int PARTTIME_HOUR, int ABSENT)
         {
-            int EMPLOYEE_WEGES_PER_HOUR = 20;
-            int FULL_DAY_HOUR = 8;
-            int PARTTIME_HOUR = 4;
-            int ABSENT = 0;
-            int EMPLOYEE_PRESENT_OR_ABSENT;
-            int PARTTIME_OR_FULLTIME;
-            int EMPLOYEE_WEGES;
-            employeeWeges emp = new employeeWeges();
-            EMPLOYEE_PRESENT_OR_ABSENT = emp.checkAttendance();
-
-        switch (EMPLOYEE_PRESENT_OR_ABSENT)
-        {
-            case 0:
-                PARTTIME_OR_FULLTIME = emp.checkAttendance();
-                switch(PARTTIME_OR_FULLTIME)
+            int EMPLOYEE_PRESENT_OR_ABSENT=0;
+            int PARTTIME_OR_FULLTIME=0;
+            while (countDays < WORKING_DAYS)
+            {
+                EMPLOYEE_PRESENT_OR_ABSENT = this.checkAttendance();
+                switch(EMPLOYEE_PRESENT_OR_ABSENT)
                 {
-                    case 0:
-                    EMPLOYEE_WEGES = emp.calculateEmployeeDailyWeges(EMPLOYEE_WEGES_PER_HOUR, FULL_DAY_HOUR);
-                        Console.WriteLine(EMPLOYEE_WEGES);
-                        break;
+                case 0:
+                    PARTTIME_OR_FULLTIME = this.checkAttendance();
+                    switch (PARTTIME_OR_FULLTIME)
+                    {
+                        case 0:
+                            EMPLOYEE_WEGES = this.calculateEmployeeDailyWeges(EMPLOYEE_WEGES_PER_HOUR, FULL_DAY_HOUR);                          
+                            break;
 
-                    case 1:
-                    EMPLOYEE_WEGES = emp.calculateEmployeeDailyWeges(EMPLOYEE_WEGES_PER_HOUR, PARTTIME_HOUR);
-                        Console.WriteLine(EMPLOYEE_WEGES);
-                        break;
+                        case 1:
+                            EMPLOYEE_WEGES = this.calculateEmployeeDailyWeges(EMPLOYEE_WEGES_PER_HOUR, PARTTIME_HOUR);
+                            break;
 
-                    default:
+                        default:
                         break;
+                    }
+                    break;
+
+                case 1:
+                    EMPLOYEE_WEGES = ABSENT;
+                    break;
                 }
-                break;
-
-            case 1:
-                EMPLOYEE_WEGES = ABSENT;
-                Console.WriteLine(EMPLOYEE_WEGES);
-                break;
-
-            default:
-               break;
-
-        }
- 
+                employee_Monthly_weges += EMPLOYEE_WEGES;
+                countDays++;
+            }
+        return employee_Monthly_weges;
         }
     }
+
+class MainD
+{
+    //private static int EMPLOYEE_PRESENT_OR_ABSENT;
+    public static void Main(string[] args)
+    {
+     
+        int EMPLOYEE_WEGES_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int PARTTIME_HOUR = 4;
+        int ABSENT = 0;
+      
+        employeeWeges emp = new employeeWeges();
+        int MONTHLY_WEGES= emp.monthlyWeges(EMPLOYEE_WEGES_PER_HOUR, FULL_DAY_HOUR, PARTTIME_HOUR, ABSENT);
+        Console.WriteLine(MONTHLY_WEGES);
+      
+    }
+}
