9ed0d48ad5552ea5e3bd0caf2cb0d34357a20c26
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-01 18:32:49 +0530

    refactor:-seprate working hour function

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+namespace employeeComputation
+{
     public class employeeWeges
     {
-    int EMPLOYEE_WEGES;
-    int employee_Monthly_weges;
-    int countDays;
-    int WORKING_DAYS;
-    int HOURS;
-    int TOTAL_MONTHLY_HOUR;
-    public employeeWeges()
-    {
-        EMPLOYEE_WEGES=0;
-        employee_Monthly_weges=0;
-        countDays = 0;
-        WORKING_DAYS = 20;
-        HOURS = 0;
-        TOTAL_MONTHLY_HOUR = 100;
-    }
-    public int checkAttendance()
+        int EMPLOYEE_WEGES;
+        int employee_Monthly_weges;
+        int countDays;
+        int WORKING_DAYS;
+        int HOURS;
+        int TOTAL_MONTHLY_HOUR;
+        public employeeWeges()
+        {
+            EMPLOYEE_WEGES = 0;
+            employee_Monthly_weges = 0;
+            countDays = 0;
+            WORKING_DAYS = 20;
+            HOURS = 0;
+            TOTAL_MONTHLY_HOUR = 100;
+        }
+        public int checkAttendance()
         {
             Random rand = new Random();
             int num = rand.Next(2);
@@ -29,75 +32,79 @@
                 return 1;
             }
         }
-        
+
         public int calculateEmployeeDailyWeges(int EMPLOYEE_WEGES_PER_HOUR, int FULL_DAY_HOUR)
         {
-            return EMPLOYEE_WEGES_PER_HOUR * FULL_DAY_HOUR; 
+            return EMPLOYEE_WEGES_PER_HOUR * FULL_DAY_HOUR;
         }
 
         public int getWorkingHours(int FULL_DAY_HOUR, int PARTTIME_HOUR)
         {
-            int  PARTTIME_OR_FULLTIME = 0;
+            int PARTTIME_OR_FULLTIME = 0;
             PARTTIME_OR_FULLTIME = this.checkAttendance();
-                switch (PARTTIME_OR_FULLTIME)
-                {
-                    case 0:
-                         HOURS = FULL_DAY_HOUR;
-                         break;
-
-                    case 1:
-                         HOURS = PARTTIME_HOUR;
-                        break;
-
-                    default:
+            switch (PARTTIME_OR_FULLTIME)
+            {
+                case 0:
+                    HOURS = FULL_DAY_HOUR;
                     break;
-                }
-        return HOURS;
-        
-    }
-      
+
+                case 1:
+                    HOURS = PARTTIME_HOUR;
+                    break;
+
+                default:
+                    break;
+            }
+            return HOURS;
+
+        }
+
         public int monthlyWeges(int EMPLOYEE_WEGES_PER_HOUR, int FULL_DAY_HOUR, int PARTTIME_HOUR, int ABSENT)
         {
-            int EMPLOYEE_PRESENT_OR_ABSENT=0;
+            int EMPLOYEE_PRESENT_OR_ABSENT = 0;
             int totalHours = 0;
             int workingHours = 0;
-            while (countDays != WORKING_DAYS && totalHours != TOTAL_MONTHLY_HOUR )
+            while (countDays != WORKING_DAYS && totalHours != TOTAL_MONTHLY_HOUR)
             {
                 EMPLOYEE_PRESENT_OR_ABSENT = this.checkAttendance();
-                switch(EMPLOYEE_PRESENT_OR_ABSENT)
+                switch (EMPLOYEE_PRESENT_OR_ABSENT)
                 {
                     case 0:
                         workingHours = getWorkingHours(FULL_DAY_HOUR, PARTTIME_HOUR);
                         totalHours += workingHours;
                         EMPLOYEE_WEGES = this.calculateEmployeeDailyWeges(EMPLOYEE_WEGES_PER_HOUR, workingHours);
-                       
+
                         break;
 
                     case 1:
                         EMPLOYEE_WEGES = ABSENT;
                         break;
                 }
-                employee_Monthly_weges += EMPLOYEE_WEGES;
+
                 countDays++;
-           
+               
+                employee_Monthly_weges += EMPLOYEE_WEGES;
             }
-        Console.WriteLine("total weges=" + employee_Monthly_weges + "days=" + countDays + "hours=" + totalHours);
-        return employee_Monthly_weges;
+            Console.WriteLine("total weges=" + employee_Monthly_weges + "days=" + countDays + "hours=" + totalHours);
+            return employee_Monthly_weges;
         }
     }
 
-class MainD
-{
-    //private static int EMPLOYEE_PRESENT_OR_ABSENT;
-    public static void Main(string[] args)
+    class MainD
     {
-        int EMPLOYEE_WEGES_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        int PARTTIME_HOUR = 4;
-        int ABSENT = 0;
-        employeeWeges emp = new employeeWeges();
-        int MONTHLY_WEGES= emp.monthlyWeges(EMPLOYEE_WEGES_PER_HOUR, FULL_DAY_HOUR, PARTTIME_HOUR, ABSENT);
-        Console.WriteLine(MONTHLY_WEGES);
-      
+        //private static int EMPLOYEE_PRESENT_OR_ABSENT;
+        public static void Main(string[] args)
+        {
+                
+            int EMPLOYEE_WEGES_PER_HOUR = 20;
+            int FULL_DAY_HOUR = 8;
+            int PARTTIME_HOUR = 4;
+            int ABSENT = 0;
+            employeeWeges emp = new employeeWeges();
+           
+            int MONTHLY_WEGES = emp.monthlyWeges(EMPLOYEE_WEGES_PER_HOUR, FULL_DAY_HOUR, PARTTIME_HOUR, ABSENT);
+            Console.WriteLine(MONTHLY_WEGES);
+            
+        }
     }
 }
